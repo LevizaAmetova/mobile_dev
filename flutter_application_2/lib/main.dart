3221b2c1c65b1d8aed57df7129087cc44d90c1f8
LevizaAmetova <leviza.ametova.06@mail.ru>
2025-10-20 14:47:03 +0000

    лабораторная работа № 2

---
+++ flutter_application_2/lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Column(
+          children: [
+            Expanded(
+              child: Container(
+                color: Colors.grey[200],
+                child: const Center(
+                  child: CircleAvatar(
+                    radius: 50,
+                    backgroundImage: NetworkImage(
+                      'https://picsum.photos/200',
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                color: Colors.white,
+                child: const Center(
+                  child: Text('Content Area'),
+                ),
+              ),
+            ),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            print(123);
+          },
+          child: const Icon(Icons.add),
+        ),
+      ),
+    );
+  }
+}
