3e1004cf1feb27d622dd9bcacb40ae22bccf0268
LevizaAmetova <leviza.ametova.06@mail.ru>
2025-11-07 10:27:36 +0000

    feat: add basic layout

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,24 +1,101 @@
-import 'dart:convert';
-import 'package:http/http.dart' as http;
+import 'package:flutter/material.dart';
 
-void main() async
-{
-  var persons= await http.get(Uri.parse('https://fakerapi.it/api/v2/persons?_locale=ru_RU'));
-  print(persons);
-  //print('body:');
-  //print(persons.body);
-
-  var data = jsonDecode(persons.body);
-  print('Статус: ${data['status']}');
-  print('Код: ${data['code']}');
-  print('Всего записей: ${data['total']}');
-  print('--- Люди ---');
-  print(data["data"]);
-
-  var i=0;
-  while( i<10 )
-  {
-  print(data["data"][i]["firstname"]);
-  i++;
+void main() {
+  runApp(const MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Lab 1',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter Lab 1 - Basic UI'),
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: Column(
+        children: [
+          // Первый контейнер
+          Container(
+            width: double.infinity,
+            height: 100,
+            color: Colors.red[300],
+            child: const Center(
+              child: Text(
+                'Верхний контейнер',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+          
+          // Ряд с текстом
+          Container(
+            margin: const EdgeInsets.symmetric(vertical: 20),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Лево',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.grey[700],
+                  ),
+                ),
+                Text(
+                  'Центр',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.grey[700],
+                  ),
+                ),
+                Text(
+                  'Право',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.grey[700],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          
+          // Второй контейнер
+          Container(
+            width: 200,
+            height: 150,
+            color: Colors.green[300],
+            child: const Center(
+              child: Text(
+                'Нижний контейнер',
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
